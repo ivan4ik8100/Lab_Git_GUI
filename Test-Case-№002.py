114132bf5b1ef1a419805b2a3a8c5a4d3c5db7ef
unknown <242909073091-1>
2026-05-08 13:41:19 +0300

    Rename PlayBaza to PlayBaza1

--- Test-Case-№002.py
+++ Test-Case-№002.py
@@ -12,7 +12,7 @@ class GoogleTestCase(unittest.TestCase):
         driver = self.driver
         driver.get('http://ivan4ik810.temp.swtest.ru/Regist.html')
         
-        self.assertIn('PlayBaza', driver.title)
+        self.assertIn('PlayBaza1', driver.title)
 
         driver.find_element(By.ID, "registr_login_input").send_keys("ИванИванИванИванИванИванИ")
         driver.find_element(By.ID, "registr_email_input").send_keys("12312312312312312312312312312@123.2")
